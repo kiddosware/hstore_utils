167cc04c2d4919100bebd6bc828565ecfab622cc
Luis Cruz <luis.cruz@escolinhas.pt>
2012-05-24 14:57:39 +0100

    fixed << bug

--- lib/hstore_utils.rb
+++ lib/hstore_utils.rb
@@ -23,20 +23,23 @@ module HstoreUtils
       attr_accessible key
       #scope "has_#{key}", lambda { |value| where("#{hstore_field} @> (? => ?)", key, value) }
 
+      define_method("#{key}=") do |value|
+        self.send("#{hstore_field}=", (self.send(hstore_field) || {}).merge(key.to_s => value))
+      end
+
       define_method(key) do
         if send(hstore_field)
           value = eval("#{hstore_field}[\"#{key}\"]")
           if (value.class.to_s == "String")
-            #hstore stores values as strings but in this case we have an array as a string, so we have to convert it (without using eval!)
-            return value[1..-2].split(',').map {|s| s.to_i}
+            #hstore stores values as strings but in this case we have an array of ints as a string, so we have to convert it (without using eval!)
+            value = value[1..-2].split(',').map {|s| s.to_i}
+            return self.send("#{key}=", value)
           end
           return value
         end
       end
 
-      define_method("#{key}=") do |value|
-        self.send("#{hstore_field}=", (self.send(hstore_field) || {}).merge(key.to_s => value))
-      end
+      
     end
   end
 end
